8e8831286e8bac8aa2a9098089d997c72de98901
Yamashita <rdai@gft.com>
2017-02-07 14:31:08 -0200

    para ajax e ng-repeat

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,8 +1,14 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
-	$scope.foto = {
-		titulo : 'Leão',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	}; 
+	$scope.fotos = [];
+
+	var promise = $http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	})
+	.error(function(erro){
+		console.log(erro);
+	})
+	
 
 });
