95ceaca05cb070df3bc8fe2a6f57501c67c44f71
Yamashita <rdai@gft.com>
2017-02-09 14:22:09 -0200

    adição do combo Grupo

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -6,22 +6,25 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
 	$http.get('/v1/fotos')
 	.success(function(retorno) {
-		$scope.fotos = retorno; 
+		console.log(retorno);
+		$scope.fotos = retorno; // não precisa fazer retorno.data
 	})
 	.error(function(erro) {
 		console.log(erro);
 	});
 
-	$scope.remover = function(foto){
-		$http.delete('v1/fotos/'+ foto._id)
-		.success(function(){
-			var indiceFoto = $scope.fotos.indexOf(foto);
-			$scope.fotos.splice(indiceFoto,1);
-			$scope.mensagem = 'Foto'+ foto.titulo +'removida com sucesso';
+	$scope.remover = function(foto) {
+
+		$http.delete('/v1/fotos/' + foto._id)
+		.success(function() {
+			var indiceDaFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceDaFoto, 1);
+			$scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
 		})
-		.error(function(erro){
+		.error(function(erro) {
 			console.log(erro);
-			$scope.mensagem = 'Foto'+ foto.titulo +' não removida';
-		})
-	}
+			$scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+		});
+	};
+
 });
