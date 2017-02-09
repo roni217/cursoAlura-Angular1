178be5f7dfcf298b9f487f33f71fd7f0a5998f53
Yamashita <rdai@gft.com>
2017-02-09 10:29:54 -0200

    Adicionado remover e editar

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,23 +1,45 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, $http) {
+.controller('FotoController', function($scope, $http, $routeParams) {
 
-        $scope.foto = {};
+	$scope.foto = {};
+	$scope.mensagem = '';
 
-        $scope.submeter = function() {
+	if($routeParams.fotoId) {
+		$http.get('v1/fotos/' + $routeParams.fotoId)
+		.success(function(foto){
+			$scope.foto = foto;
+		})
+		.error(function(erro){
+			console.log(erro);
+			$scope.mensagem = "Não foi possível obter a tofo";
 
-            if ($scope.formulario.$valid) {
+		})
+	}
 
-                $http.post('/v1/fotos', $scope.foto)
-                .success(function() {
-                	$scope.foto = {};
-                	$scope.mensagem = 'Foto adicionada com sucesso';
-                    console.log('Foto adicionada com sucesso');
-                })
-                .error(function(erro) {
-                    $scope.mensagem = 'Não foi possível cadastra a foto';
-                    console.log('Não foi possível cadastra a foto');
-                })
-            }
-        };
+	$scope.submeter = function() {
+		if ($scope.formulario.$valid) {
+			if($scope.foto._id){
+				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+				.success(function(){
+					$scope.mensagem = 'A foto'+ $scope.foto.titulo +' foi possível alterar a foto';
+				})
+				.error(function(erro){
+					$scope.mensagem = 'Não foi possível alterar a foto';
+					console.log(erro);
+				})
+			} else {
+				$http.post('/v1/fotos', $scope.foto)
+				.success(function() {
+					$scope.foto = {};
+					$scope.mensagem = 'Foto adicionada com sucesso';
+					console.log('Foto adicionada com sucesso');
+				})
+				.error(function(erro) {
+					$scope.mensagem = 'Não foi possível cadastra a foto';
+					console.log(erro);
+				});
+			}
+		}
+	};
 
-    });
+});
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,14 +2,26 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	
 	$scope.fotos = [];
 	$scope.filtro = '';
+	$scope.mensagem = '';
 
 	$http.get('/v1/fotos')
 	.success(function(retorno) {
-		console.log(retorno);
-		$scope.fotos = retorno; // não precisa fazer retorno.data
+		$scope.fotos = retorno; 
 	})
 	.error(function(erro) {
 		console.log(erro);
 	});
 
+	$scope.remover = function(foto){
+		$http.delete('v1/fotos/'+ foto._id)
+		.success(function(){
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto,1);
+			$scope.mensagem = 'Foto'+ foto.titulo +'removida com sucesso';
+		})
+		.error(function(erro){
+			console.log(erro);
+			$scope.mensagem = 'Foto'+ foto.titulo +' não removida';
+		})
+	}
 });
