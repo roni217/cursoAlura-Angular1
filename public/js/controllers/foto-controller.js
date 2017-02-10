3c08158afabbff085e08d7a4a2587dea17d86877
Yamashita <rdai@gft.com>
2017-02-10 13:51:00 -0200

    resolvendo problema com as URL's espalhadas criando um servico

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,47 +1,42 @@
 angular.module('alurapic')
-	.controller('FotoController', function($scope, $http, $routeParams) {
+    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
 
-		$scope.foto = {};
-		$scope.mensagem = '';
+        $scope.foto = {};
+        $scope.mensagem = '';
 
-		if($routeParams.fotoId) {
-			$http.get('/v1/fotos/' + $routeParams.fotoId)
-			.success(function(foto) {
-				$scope.foto = foto;
-			})
-			.error(function(erro) {
-				console.log(erro);
-				$scope.mensagem = 'Não foi possível obter a foto'
-			});
-		}
+        if($routeParams.fotoId) {
+            recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
+                $scope.foto = foto; 
+            }, function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });
+        }
 
-		$scope.submeter = function() {
+        $scope.submeter = function() {
 
-			if ($scope.formulario.$valid) {
+            if ($scope.formulario.$valid) {
 
-				if($routeParams.fotoId) {
+                if($routeParams.fotoId) {
 
-					$http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-					.success(function() {
-						$scope.mensagem = 'Foto alterada com sucesso';
-					})
-					.error(function(erro) {
-						console.log(erro);
-						$scope.mensagem = 'Não foi possível alterar';
-					});
+                    recursoFoto.update({fotoId: $scope.foto._id}, 
+                        $scope.foto, function() {
+                        $scope.mensagem = 'Foto alterada com sucesso';
+                    }, function() {
+                        console.log(erro);
+                        $scope.mensagem = 'Não foi possível alterar';
+                    });
 
-				} else { 
+                } else {
+                    recursoFoto.save($scope.foto, function() {
+                        $scope.foto = {};
+                        $scope.mensagem = 'Foto cadastrada com sucesso';
+                    }, function(erro) {
+                        console.log(erro);
+                        $scope.mensagem = 'Não foi possível cadastrar a foto';
+                    });
+                }
+            }
+        };
 
-					$http.post('/v1/fotos', $scope.foto)
-					.success(function() {
-						$scope.foto = {};
-						$scope.mensagem = 'Foto cadastrada com sucesso';
-					})
-					.error(function(erro) {
-						console.log(erro);
-						$scope.mensagem = 'Não foi possível cadastrar a foto';
-					})
-				}
-			}
-		};
-	});
+    });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,27 +1,24 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http) {
+angular.module('alurapic').controller('FotosController', function($scope, recursoFoto) {
 	
 	$scope.fotos = [];
 	$scope.filtro = '';
 	$scope.mensagem = '';
 
-	$http.get('/v1/fotos')
-	.success(function(retorno) {
-		console.log(retorno);
-		$scope.fotos = retorno; // não precisa fazer retorno.data
-	})
-	.error(function(erro) {
+	recursoFoto.query(function(fotos) {
+		$scope.fotos = fotos;
+	}, function(erro) {
 		console.log(erro);
 	});
 
+	
+
 	$scope.remover = function(foto) {
 
-		$http.delete('/v1/fotos/' + foto._id)
-		.success(function() {
+		recursoFoto.delete({fotoId : foto._id}, function(){
 			var indiceDaFoto = $scope.fotos.indexOf(foto);
 			$scope.fotos.splice(indiceDaFoto, 1);
 			$scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
-		})
-		.error(function(erro) {
+		},function(erro){
 			console.log(erro);
 			$scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
 		});
